53f987183b2161f6abf9a99b5e48b6ffe1ed13bd
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-04-27 21:59:08 -0300

    email auth config

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,8 @@
 class SessionsController < ApplicationController
+  def create
+    user_email = params['user']['email']
+    user_password = params['user']['password']
+
+    user = User.find_by(email: user_email).try(:authenticate, user_password)
+  end
 end
