d861c6a4308582ccdbc938bdbcd114f5cdd0b3c2
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-04-27 22:01:51 -0300

    session auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,17 @@ class SessionsController < ApplicationController
     user_password = params['user']['password']
 
     user = User.find_by(email: user_email).try(:authenticate, user_password)
+
+    if user
+      session[:user_id] = user.id
+
+      render json: {
+        status: :created,
+        logged_in: true,
+        user: user
+      }
+    else
+      render json: { status: 401 }
+    end
   end
 end
