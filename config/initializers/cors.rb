ad75deff07c7ba5c44dc9a958ae060d276bbb01b
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-04-27 21:41:16 -0300

    cors config

---
+++ config/initializers/cors.rb
@@ -0,0 +1,11 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origin "http://localhost:3000"
+    resource "*", headers: any, methods: %i[get put patch post delete options head], credentials: true
+  end
+
+  # allow do
+  #   origin "http://localhost:3000" production domain
+  #   resource "*", headers: any, methods: %i[get put patch post delete options head], credentials: true
+  # end
+end
