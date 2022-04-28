4fada45f5ea2ac9199315db47b9854034d405cb3
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-04-27 22:43:57 -0300

    loggedout method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -30,4 +30,10 @@ class SessionsController < ApplicationController
       render json: { logged_in: false }
     end
   end
+
+  def logout
+    reset_session
+    render json: { status: 200, logged_out: true }
+  end
+
 end
